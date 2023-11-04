6df99811abb5dedfd8beb336e61912b7cefcd994
Han Song Leeeeeee <hlf1850@students.uc-bcf.edu.ph>
2023-11-04 22:17:47 +0800

    changes in javascript

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
 const taskInput = document.getElementById('taskInput');
-        const groupSelect = document.getElementById('groupSelect');
-        const addTaskButton = document.getElementById('addTask');
-        const taskList = document.getElementById('taskList');
+const groupSelect = document.getElementById('groupSelect');
+const addTaskButton = document.getElementById('addTask');
+const taskList = document.getElementById('taskList');
+const progressBar = document.getElementById('progressBar'); // Assuming you have a progress bar element in your HTML
 
-        addTaskButton.addEventListener('click', () => {
-            const taskText = taskInput.value;
-            const group = groupSelect.value;
+let completedTasks = 0;
+let totalTasks = 0;
 
-            if (taskText.trim() !== '') {
-                const task = document.createElement('li');
-                task.className = 'task';
-                task.innerHTML = `
-                    <span>${taskText}</span>
-                    <button>Finish</button>
-                `;
+addTaskButton.addEventListener('click', () => {
+    const taskText = taskInput.value;
+    const group = groupSelect.value;
 
-                // Add event listener to delete task
-                task.querySelector('button').addEventListener('click', () => {
-                    task.remove();
-                });
+    if (taskText.trim() !== '') {
+        const task = document.createElement('li');
+        task.className = 'task';
+        task.innerHTML = `
+            <span>${taskText}</span>
+            <button>Finish</button>
+        `;
 
-                taskList.appendChild(task);
-                taskInput.value = '';
-                groupSelect.value = 'default';
-            }
+        // Add event listener to delete task and update progress
+        task.querySelector('button').addEventListener('click', () => {
+            task.remove();
+            completedTasks++;
+            updateProgress();
         });
+
+        taskList.appendChild(task);
+        taskInput.value = '';
+        groupSelect.value = 'default';
+        totalTasks++;
+        updateProgress();
+    }
+
+    function updateProgress() {
+        if (totalTasks > 0) {
+            const percentage = (completedTasks / totalTasks) * 100;
+            progressBar.style.width = percentage + '%';
+        } else {
+            progressBar.style.width = '0%';
+        }
+    }
+});
+
+
