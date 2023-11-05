892ce69668afdfe4ab9d24a82e9291cd9a352b3b
Han Song Leeeeeee <hlf1850@students.uc-bcf.edu.ph>
2023-11-05 14:58:08 +0800

    Changes in javasscript

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const taskInput = document.getElementById('taskInput');
-const groupSelect = document.getElementById('groupSelect');
 const addTaskButton = document.getElementById('addTask');
 const taskList = document.getElementById('taskList');
-const progressBar = document.getElementById('progressBar'); // Assuming you have a progress bar element in your HTML
+const progressBar = document.getElementById('myBar'); 
 
 let completedTasks = 0;
 let totalTasks = 0;
 
 addTaskButton.addEventListener('click', () => {
     const taskText = taskInput.value;
-    const group = groupSelect.value;
+    totalTasks++;
+    updateProgress();
 
     if (taskText.trim() !== '') {
         const task = document.createElement('li');
@@ -19,7 +19,6 @@ addTaskButton.addEventListener('click', () => {
             <button>Finish</button>
         `;
 
-        // Add event listener to delete task and update progress
         task.querySelector('button').addEventListener('click', () => {
             task.remove();
             completedTasks++;
@@ -29,18 +28,14 @@ addTaskButton.addEventListener('click', () => {
         taskList.appendChild(task);
         taskInput.value = '';
         groupSelect.value = 'default';
-        totalTasks++;
-        updateProgress();
-    }
-
-    function updateProgress() {
-        if (totalTasks > 0) {
-            const percentage = (completedTasks / totalTasks) * 100;
-            progressBar.style.width = percentage + '%';
-        } else {
-            progressBar.style.width = '0%';
-        }
     }
 });
 
-
+function updateProgress() {
+    if (totalTasks > 0) {
+        const percentage = (completedTasks / totalTasks) * 100;
+        progressBar.style.width = percentage + '%';        
+    } else {
+        progressBar.style.width = '0%';
+    }
+}
